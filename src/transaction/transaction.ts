45f5dd114fcd899c177ac556470231bb26faa41a
Kazuki Kasahara <nuderebatsin.roal@gmail.com>
2022-06-28 17:39:53 +0400

    sign a transaction with max length of 192

---
+++ src/transaction/transaction.ts
@@ -0,0 +1,37 @@
+import * as circomlibjs from "circomlibjs";
+import { Scalar } from "ffjavascript";
+import { Buffer } from "buffer";
+import * as crypto from "crypto";
+import { toHex } from "../utils";
+
+interface TxData {
+  from?: string;
+  to?: string;
+  data?: {
+    [key: string]: string;
+  };
+}
+
+export class Transaction {
+  public data: TxData;
+
+  constructor(data: TxData = {}) {
+    this.data = data;
+  }
+
+  async sign(privateKey: string) {
+    const eddsa = await circomlibjs.buildEddsa();
+
+    const buffer = Buffer.from(JSON.stringify(this.data));
+    const msgHashed = crypto.createHash("sha256").update(buffer).digest();
+    const msg = eddsa.babyJub.F.e(Scalar.fromRprLE(msgHashed, 0));
+
+    const prvKey = Buffer.from(privateKey, "hex");
+    const signature = eddsa.signPoseidon(prvKey, msg);
+
+    return {
+      R8: signature.R8.map(toHex),
+      S: `0x${signature.S}`,
+    };
+  }
+}
--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,9 +1,19 @@
 import { Buffer } from "buffer";
 
+export type BufferString = Buffer | Buffer[] | string[] | string;
+
 export const toHex = (value: Buffer | string) => {
   if (typeof value === "string") {
     return value;
   }
 
   return `0x${Buffer.from(value).toString("hex")}`;
+};
+
+export const toHexFromArray = (value: BufferString) => {
+  if (Array.isArray(value)) {
+    return value.map(toHex);
+  }
+
+  return toHex(value);
 };

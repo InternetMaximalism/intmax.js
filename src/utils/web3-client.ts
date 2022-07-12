ac0580786ce334ca3a84c6f8e139d9d80f4ae653
kazuko <nuderebatsin.roal@gmail.com>
2022-07-12 15:33:21 +0400

    generated address from public key (#17)

--- src/utils/web3-client.ts
+++ src/utils/web3-client.ts
@@ -3,6 +3,7 @@ import ethers from "ethers";
 export class Web3Client {
   readonly provider: ethers.providers.JsonRpcProvider;
   readonly signer: ethers.providers.JsonRpcSigner;
+  readonly msg = "intmax sign message";
 
   constructor(ethereum: ethers.providers.ExternalProvider) {
     this.provider = new ethers.providers.Web3Provider(ethereum);
@@ -10,7 +11,7 @@ export class Web3Client {
   }
 
   async createPrivateKey() {
-    const signature = await this.signer.signMessage("something message");
+    const signature = await this.signer.signMessage(this.msg);
 
     return `0x${ethers.utils.keccak256(signature)}`;
   }

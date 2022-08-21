89bda40aac956788b94168fee792a22fbcb8c613
kazuko <nuderebatsin.roal@gmail.com>
2022-08-21 13:56:48 +0400

    pass provider to initialize web3 client (#19)

--- src/account/account.ts
+++ src/account/account.ts
@@ -1,16 +1,20 @@
 import * as circomlibjs from "circomlibjs";
-import ethers from "ethers";
+import { ethers } from "ethers";
 import { Web3Client } from "../utils";
 
 export class Account {
   static readonly storeName = "account";
   readonly web3: Web3Client;
 
-  constructor(ethereum: ethers.providers.ExternalProvider) {
+  constructor(
+    ethereum:
+      | ethers.providers.ExternalProvider
+      | ethers.providers.JsonRpcProvider
+  ) {
     this.web3 = new Web3Client(ethereum);
   }
 
-  async createAddress() {
+  async createAddress(): Promise<string> {
     const eddsa = await circomlibjs.buildEddsa();
     const privateKey = await this.web3.createPrivateKey();
     const [key] = eddsa.prvTopub(privateKey);
@@ -18,7 +22,7 @@ export class Account {
     return this.pubToAddress(key);
   }
 
-  private pubToAddress(publicKey: Buffer) {
+  private pubToAddress(publicKey: Buffer): string {
     return `0x${ethers.utils.keccak256(publicKey).slice(-40)}`;
   }
 }
--- src/utils/web3-client.ts
+++ src/utils/web3-client.ts
@@ -1,18 +1,39 @@
-import ethers from "ethers";
+import { ethers } from "ethers";
 
 export class Web3Client {
   readonly provider: ethers.providers.JsonRpcProvider;
   readonly signer: ethers.providers.JsonRpcSigner;
+  readonly isMetamask: boolean;
   readonly msg = "intmax sign message";
 
-  constructor(ethereum: ethers.providers.ExternalProvider) {
-    this.provider = new ethers.providers.Web3Provider(ethereum);
+  constructor(
+    ethereum:
+      | ethers.providers.ExternalProvider
+      | ethers.providers.JsonRpcProvider
+  ) {
+    this.isMetamask = (ethereum as any).isMetamask;
+    this.provider = this.isMetamask
+      ? new ethers.providers.Web3Provider(
+          ethereum as ethers.providers.ExternalProvider
+        )
+      : (ethereum as ethers.providers.JsonRpcProvider);
+
     this.signer = this.provider.getSigner();
   }
 
-  async createPrivateKey() {
-    const signature = await this.signer.signMessage(this.msg);
+  async createPrivateKey(): Promise<string> {
+    const signature = await this.getSignature();
 
     return `0x${ethers.utils.keccak256(signature)}`;
+  }
+
+  async getSignature(): Promise<string> {
+    if (this.isMetamask) {
+      return await this.signer.signMessage(this.msg);
+    }
+
+    const wallet = ethers.Wallet.createRandom();
+
+    return await wallet.signMessage(this.msg);
   }
 }
